b283245684a7fb5137f5c20a3176e0b4d88193ea
Michael Bock <no-day@posteo.net>
2023-11-26 14:58:05 +0100

    Fix test

--- test/MarkedSpec.purs
+++ test/MarkedSpec.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
+import Marked (Token(..))
 import Marked as ME
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (shouldEqual)
@@ -11,69 +12,110 @@ import Test.Spec.Assertions (shouldEqual)
 spec :: Spec Unit
 spec = describe "Marked" $ do
   describe "lexer" $ do
-    -- it "works" $ do
-    --   ME.parse " " `shouldEqual` (Right [ ME.MdSpace ])
-    it "Code" do
-      ME.lexer "```\nfoo\n```"
-        `shouldEqual`
-          (Right [ ME.TokCode { text: "foo", lang: Just "" } ])
-
-    it "Heading" do
-      ME.lexer "#"
-        `shouldEqual`
-          (Right [ ME.TokHeading { depth: 1, tokens: [] } ])
-      ME.lexer "##"
-        `shouldEqual`
-          (Right [ ME.TokHeading { depth: 2, tokens: [] } ])
-
-    it "Paragraph" do
-      ME.lexer "hello"
-        `shouldEqual`
-          (Right [ (ME.TokParagraph { tokens: [ (ME.TokText { text: "hello" }) ] }) ])
-
-    it "Document" do
+    it "Document1" do
       ME.lexer doc1
         `shouldEqual`
-          ( Right
-              [ ME.TokSpace
-              , ( ME.TokHeading
-                    { depth: 1
-                    , tokens:
-                        [ (ME.TokText { text: "Heading1" })
-                        ]
-                    }
-                )
-              , ( ME.TokParagraph
-                    { tokens:
-                        [ (ME.TokText { text: "Hello!" })
-                        ]
-                    }
-                )
-              , ME.TokSpace
-              , ( ME.TokHeading
-                    { depth: 2
-                    , tokens:
-                        [ (ME.TokText { text: "Heading2" })
-                        ]
-                    }
-                )
-              , ( ME.TokParagraph
-                    { tokens:
-                        [ (ME.TokText { text: "Hellooo!" })
-                        ]
-                    }
-                )
-              ]
+          ( ( Right
+                [ TokSpace
+                , (TokHeading { depth: 1, tokens: [ (TokText { text: "Markdown Sample" }) ] })
+                , (TokHeading { depth: 2, tokens: [ (TokText { text: "Headers" }) ] })
+                , (TokHeading { depth: 3, tokens: [ (TokText { text: "Subheaders" }) ] })
+                , (TokParagraph { tokens: [ (TokText { text: "This is a paragraph with some " }), (TokStrong {}), (TokText { text: " and " }), (TokEm {}), (TokText { text: ". Also, there&#39;s inline " }), (TokCodespan {}), (TokText { text: "." }) ] })
+                , TokSpace
+                , (TokList {})
+                , TokSpace
+                , (TokList {})
+                , TokSpace
+                , (TokBlockquote {})
+                , (TokParagraph { tokens: [ (TokLink {}) ] })
+                , TokSpace
+                , (TokParagraph { tokens: [ (TokImage {}) ] })
+                , TokSpace
+                , (TokHr {})
+                , (TokParagraph { tokens: [ (TokText { text: "Horizontal rule" }) ] })
+                , TokSpace
+                , (TokParagraph { tokens: [ (TokCodespan {}), (TokText { text: ":" }) ] })
+                , TokSpace
+                , (TokCode { lang: (Just "python"), text: "def hello_world():\n    print(\"Hello, world!\")" })
+                , TokSpace
+                , (TokParagraph { tokens: [ (TokText { text: "Tables:" }) ] })
+                , TokSpace
+                , (TokTable {})
+                , (TokParagraph { tokens: [ (TokText { text: "Checkboxes:" }) ] })
+                , TokSpace
+                , (TokList {})
+                , TokSpace
+                , (TokParagraph { tokens: [ (TokText { text: "Footnote[^1] and inline footnote^[Inline Footnote]." }) ] })
+                , TokSpace
+                , (TokParagraph { tokens: [ (TokText { text: "[^1]: This is a footnote." }) ] })
+                , TokSpace
+                , (TokParagraph { tokens: [ (TokText { text: "Mathematics: " }), (TokEscape {}), (TokText { text: "E=mc^2" }), (TokEscape {}) ] })
+                , TokSpace
+                , (TokHr {})
+                , (TokParagraph { tokens: [ (TokText { text: "End of the Markdown sample." }) ] })
+                , TokSpace
+                ]
+            )
           )
 
 doc1 :: String
 doc1 =
   """
-# Heading1
+# Markdown Sample
 
-Hello!
+## Headers
 
-## Heading2
+### Subheaders
 
-Hellooo!
+This is a paragraph with some **bold text** and *italic text*. Also, there's inline `code`.
+
+- Unordered list item 1
+- Unordered list item 2
+  - Subitem 1
+  - Subitem 2
+
+1. Ordered list item 1
+2. Ordered list item 2
+   1. Subitem A
+   2. Subitem B
+
+> This is a blockquote.
+
+[Link to Google](https://www.google.com)
+
+![Image Alt Text](https://example.com/image.jpg)
+
+---
+
+Horizontal rule
+
+`Code block`:
+
+```python
+def hello_world():
+    print("Hello, world!")
+```
+
+Tables:
+
+| Header 1 | Header 2 |
+|----------|----------|
+| Cell 1   | Cell 2   |
+| Cell 3   | Cell 4   |
+
+Checkboxes:
+
+- [x] Task 1
+- [ ] Task 2
+
+Footnote[^1] and inline footnote^[Inline Footnote].
+
+[^1]: This is a footnote.
+
+Mathematics: \(E=mc^2\)
+
+---
+
+End of the Markdown sample.
+
 """
